dfb1327b85bbced0cbe406d5c52b136d5f8a80b8
Liew Chen Tzer <liewct115@gmail.com>
2024-07-26 22:55:42 +0800

    (Spring 5) [BASE PROJECT CREATION] 1) Dependency injection - Autowired a) put it on constructor/ setter depending on requirements

---
+++ src/main/java/car/Engine.java
@@ -0,0 +1,9 @@
+package car;
+
+import org.springframework.stereotype.Component;
+
+@Component("engine")
+public class Engine {
+    String type = "V8";
+
+}
--- src/main/java/car/Swift.java
+++ src/main/java/car/Swift.java
@@ -1,13 +1,16 @@
 package car;
 
 import interfaces.Car;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("swift")
 public class Swift implements Car {
+    @Autowired(required = false)
+    Engine engine;
 
     @Override
     public void specs() {
-        System.out.println("Hatchback from Suzuki");
+        System.out.println("Hatchback from Suzuki with engine as " + engine.type);
     }
 }
